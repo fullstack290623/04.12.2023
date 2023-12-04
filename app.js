5ba4907348cd648814331305dc0a097febcc21d8
fullstack290623 <fullstack290623@gmail.com>
2023-12-04 21:03:10 +0200

    .

--- app.js
+++ app.js
@@ -11,12 +11,39 @@ logger.info('==== System start =======')
 const app = express()
 const port = 3000
 app.use(body_parser.json())
-app.use(express.static(path.join('.', '/static/'))) 
+app.use(express.static(path.join('.', '/static/')))
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 app.listen(3000, () => {
     logger.info('==== Server started =======')
     console.log('Express server is running ....');
 })
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Library API",
+            version: "1.0.0",
+            description: "My REST API employee",
+        },
+        servers: [
+            {
+                url: "https://my-cloud1.onrender.com/",
+            },
+        ],
+    },
+    apis: ["./routers/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
 app.use('/api/employee', employee_router)
 
--- routers/employee_router.js
+++ routers/employee_router.js
@@ -2,14 +2,65 @@ const express = require('express')
 const router = express.Router()
 const dal = require('../dals/dal')
 
+/**
+*  @swagger
+*  components:
+*     schemas:
+*       Employee:
+*         type: object
+*         required:
+*           - id
+*           - name
+*           - age
+*           - address
+*           - salary
+*         properties:
+*           id:
+*             type: number
+*             description: The auto-generated id of the employee.
+*           name:
+*             type: string
+*             description: The name of the employee.
+*           age:
+*             type: number
+*             description: age of the employee
+*           address:
+*             type: string
+*             description: The address of the employee.
+*           salary:
+*             type: number
+*             description: salary of the employee
+*         example:
+*           name: Kim
+*           age: 22
+*           address: South-Hall
+*           salary: 45000
+*/
+
+
+
 // '/api/employee'
 // GET 
+
 router.get('/hello', async (request, response) => {
     response.json({'status': 'success'})
 })
 
 // '/api/employee'
 // GET 
+/**
+*  @swagger
+*   /api/employee/:
+*     get:
+*       summary: List all of the employees
+*       responses:
+*         "200":
+*           description: The list of employees.
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/Employee'
+*/
 router.get('/', async (request, response) => {
     try {
     const employees = await dal.get_all()
@@ -20,6 +71,37 @@ router.get('/', async (request, response) => {
     }
 })
 // GET by ID
+/**
+ * @swagger
+ * /api/employee/{id}:
+ *   get:
+ *     summary: Get an employee by ID
+ *     description: Retrieve employee details based on the provided ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the employee to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response with the employee details.
+ *         content:
+ *           application/json:
+ *             example:
+ *               ID: 1
+ *               NAME: John Doe
+ *               AGE: 30
+ *               ADDRESS: Example Street
+ *               SALARY: 50000.00
+ *       404:
+ *         description: Employee not found with the specified ID.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: cannot find employee with id {id}
+ */
 router.get('/:id', async (request, response) => {
     const user_id = parseInt(request.params.id)
     const user = await dal.get_by_id(user_id)
@@ -31,6 +113,54 @@ router.get('/:id', async (request, response) => {
     }
 })
 // POST
+/**
+ * @swagger
+ * /api/employee:
+ *   post:
+ *     summary: Create a new employee
+ *     description: Create a new employee record with the provided details.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               NAME:
+ *                 type: string
+ *                 description: The name of the employee.
+ *               AGE:
+ *                 type: number
+ *                 description: The age of the employee.
+ *               ADDRESS:
+ *                 type: string
+ *                 description: The address of the employee.
+ *               SALARY:
+ *                 type: number
+ *                 description: The salary of the employee.
+ *             example:
+ *               name: John Doe
+ *               age: 30
+ *               address: Example Street
+ *               salary: 50000.00
+ *     responses:
+ *       201:
+ *         description: Employee created successfully.
+ *         content:
+ *           application/json:
+ *             example:
+ *               ID: 1
+ *               NAME: John Doe
+ *               AGE: 30
+ *               ADDRESS: Example Street
+ *               SALARY: 50000.00
+ *       400:
+ *         description: Bad request. Ensure all required fields are provided.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Bad request. Missing required fields.
+ */
 router.post('/', async (request, response) => {
     const new_user = request.body
     const result = await dal.new_employee(new_user)
@@ -65,6 +195,29 @@ router.patch('/:id', async (request, response) => {
 })
 
 // DELETE
+/**
+ * @swagger
+ * /api/employee/{id}:
+ *   delete:
+ *     summary: Delete an employee by ID
+ *     description: Delete the employee record with the specified ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the employee to delete.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Employee deleted successfully.
+ *       404:
+ *         description: Employee not found with the specified ID.
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: cannot find employee with id {id}
+ */
 router.delete('/:id', async (request, response) => {
     const user_id = parseInt(request.params.id)
     const result = await dal.delete_employee(user_id)
