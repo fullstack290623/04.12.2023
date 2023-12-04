6160b64c93a7f5876603a6134a05c736518f93c8
fullstack290623 <fullstack290623@gmail.com>
2023-12-04 21:27:36 +0200

    Update employee_router.js

--- routers/employee_router.js
+++ routers/employee_router.js
@@ -43,7 +43,7 @@ const dal = require('../dals/dal')
 // GET 
 
 router.get('/hello', async (request, response) => {
-    response.json({'status': 'success'})
+    response.json({ 'status': 'success' })
 })
 
 // '/api/employee'
@@ -63,11 +63,11 @@ router.get('/hello', async (request, response) => {
 */
 router.get('/', async (request, response) => {
     try {
-    const employees = await dal.get_all()
-    response.json(employees)
+        const employees = await dal.get_all()
+        response.json(employees)
     }
     catch (e) {
-        response.json({'error': JSON.stringify(e)})
+        response.status(500).json({ 'error': JSON.stringify(e) })
     }
 })
 // GET by ID
@@ -162,36 +162,54 @@ router.get('/:id', async (request, response) => {
  *               error: Bad request. Missing required fields.
  */
 router.post('/', async (request, response) => {
-    const new_user = request.body
-    const result = await dal.new_employee(new_user)
-    response.status(201).json(result)
-})
-// PUT
-router.put('/:id', async (request, response) => {
-    const user_id = parseInt(request.params.id)
-    const user = await dal.get_by_id(user_id)
-    if (user) {
-        // user exists ==> perform update
-        const updated_user_req = request.body
-        const result = await dal.update_emplyee(user_id, updated_user_req)
-        response.json(updated_user_req)
-    }
-    else {
-        // user does NOT exist ==> perform insert
+    try {
         const new_user = request.body
         const result = await dal.new_employee(new_user)
         response.status(201).json(result)
     }
+    catch (e) {
+        response.status(400).json({ 'Error': e })
+    }
+})
+// PUT
+router.put('/:id', async (request, response) => {
+    try {
+        const user_id = parseInt(request.params.id)
+        const user = await dal.get_by_id(user_id)
+        if (user) {
+            // user exists ==> perform update
+            const updated_user_req = request.body
+            const result = await dal.update_emplyee(user_id, updated_user_req)
+            response.status(200).json(updated_user_req)
+        }
+        else {
+            // user does NOT exist ==> perform insert
+            const new_user = request.body
+            const result = await dal.new_employee(new_user)
+            response.status(201).json(result)
+        }
+    }
+    catch (e) {
+        response.status(400).json({ 'Error': e })
+    }
 })
 // PATCH
 router.patch('/:id', async (request, response) => {
-    const updated_user_req = request.body
-    const user_id = parseInt(request.params.id)
-    const user = await dal.get_by_id(user_id)
-    // override only existing fields in the user from the db
-    const result = await dal.update_emplyee(user_id, { ...user, ...updated_user_req })
-    response.json({ result })
-
+    try {
+        const updated_user_req = request.body
+        const user_id = parseInt(request.params.id)
+        const user = await dal.get_by_id(user_id)
+        // override only existing fields in the user from the db
+        if (!user) {
+            response.status(404).json({ "error": `cannot find user with id ${user_id}` })
+            return
+        }
+        const result = await dal.update_emplyee(user_id, { ...user, ...updated_user_req })
+        response.status(200).json({ result })
+    }
+    catch (e) {
+        response.status(400).json({ 'Error': e })
+    }
 })
 
 // DELETE
@@ -219,10 +237,14 @@ router.patch('/:id', async (request, response) => {
  *               error: cannot find employee with id {id}
  */
 router.delete('/:id', async (request, response) => {
-    const user_id = parseInt(request.params.id)
-    const result = await dal.delete_employee(user_id)
-    response.status(204).json({ result })
-
+    try {
+        const user_id = parseInt(request.params.id)
+        const result = await dal.delete_employee(user_id)
+        response.status(204).json({ result })
+    }
+    catch (e) {
+        response.status(400).json({ 'Error': e })
+    }
 })
 
 module.exports = router
